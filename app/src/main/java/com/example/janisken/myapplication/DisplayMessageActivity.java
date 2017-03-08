26f5b83d892c6812d76eba5631f2ed9c7fb70d30
janisken <janisken>
2017-03-08 16:48:24 -0600

    Initial commit

---
+++ app/src/main/java/com/example/janisken/myapplication/DisplayMessageActivity.java
@@ -0,0 +1,22 @@
+package com.example.janisken.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        textView.setText(message+" logged in!");
+    }
+}
